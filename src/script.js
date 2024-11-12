c81ddcb09c152853ccc343dfa40a826e576b4595
Ignacio C <ignacio.caldsil@gmail.com>
2024-11-12 01:04:51 -0500

    Nuevo proyecto de Javascript

---
+++ src/script.js
@@ -0,0 +1,5 @@
+// Elementos HMTL
+const board= document.getElementById('board');
+const score= document.getElementById('scoreBoard');
+const startButton= document.getElementById('start');
+const finishOver= document.getElementById('gameOver');
