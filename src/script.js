d8a3dfc8d0e34f06a65ab36a786a8f32d7e9172c
Ignacio C <ignacio.caldsil@gmail.com>
2024-11-12 23:47:11 -0500

    Segundo avance del proyecto

--- src/script.js
+++ src/script.js
@@ -1,5 +1,30 @@
 // Elementos HMTL
 const board= document.getElementById('board');
-const score= document.getElementById('scoreBoard');
+const scoreBoard= document.getElementById('scoreBoard');
 const startButton= document.getElementById('start');
 const finishOver= document.getElementById('gameOver');
+
+//Configuracion del juego
+const boardSize = 10;
+const snakeSize = 100;
+const squareTypes = {
+    emptySquare : 0,
+    snakeSquare : 1,
+    foodSquare : 2
+};
+
+const directions ={
+    ArrowUp: -10,
+    ArrowDown: 10,
+    ArrowLeft: -1,
+    ArrowRight: 1
+};
+
+// Variable de los juegos
+let snake;
+let score;
+let direction;
+let boardSquares;
+let emptySquares;
+let moveInterval;
+
